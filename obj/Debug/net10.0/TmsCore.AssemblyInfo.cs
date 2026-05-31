7717c9accc6c3a5f7a36c1e74dab4a44809b51ca
Owner <Owner@DESKTOP-TPSJ8HG>
2026-06-01 01:04:53 +0300

    feat(module-1/session-2): implement guard clauses, pattern matching and LINQ analytics

--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce47bc97bc269e298882c501c38f5b7d271ba5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
